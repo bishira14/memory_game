07f9c936d09a7d6fd4ac2b90dfd3d0c3fee80c77
Hamza Bishira <bishira12@hotmail.co.uk>
2020-04-10 21:21:38 +0100

    latest update

--- index.js
+++ index.js
@@ -4,41 +4,51 @@
   let secondCard = null;
   let lockedBoard = true;
 
+ function startGame(){
 
-  for(let i = 0; i < cards.length; i++){
-    cards[i].addEventListener('click', flipCard);
-    function flipCard(){
-      if(lockedBoard == false) return;
-      if(flippedCard) {
-        firstCard = cards[i];
-        firstCard.classList.add('flip');
-        flippedCard = false;
-      }else{
-        lockedBoard = false;
-        secondCard = cards[i];
-        secondCard.classList.add('flip');
-          if(secondCard === firstCard){
-            firstCard.classList.remove('flip');
-            secondCard.classList.remove('flip');
-            setTimeout(function(){lockedBoard = true},200);
-          } else if(firstCard.dataset.framework === secondCard.dataset.framework){
-             firstCard.removeEventListener('click', flipCard);
-             secondCard.removeEventListener('click', flipCard);
+   for(let i = 0; i < cards.length; i++){
+     (function(){
+      let random = Math.floor(Math.random()*3)
+       return cards[i].style.order = random;
+     })();
+     cards[i].addEventListener('click', flipCard);
+     function flipCard(){
+       if(lockedBoard == false) return;
+       if(flippedCard) {
+         firstCard = cards[i];
+         firstCard.classList.add('flip');
+         flippedCard = false;
+       }else{
+         lockedBoard = false;
+         secondCard = cards[i];
+         secondCard.classList.add('flip');
+           if(secondCard === firstCard){
+             firstCard.classList.remove('flip');
+             secondCard.classList.remove('flip');
              setTimeout(function(){lockedBoard = true},200);
-           }else{
-             setTimeout(function(){
-               firstCard.classList.remove('flip');
-               secondCard.classList.remove('flip');
-               lockedBoard = true;
-             }, 1000);
+           } else if(firstCard.dataset.framework === secondCard.dataset.framework){
+              firstCard.removeEventListener('click', flipCard);
+              secondCard.removeEventListener('click', flipCard);
+              setTimeout(function(){lockedBoard = true},200);
+            }else{
+              setTimeout(function(){
+                firstCard.classList.remove('flip');
+                secondCard.classList.remove('flip');
+                lockedBoard = true;
+              }, 1000);
 
-           }
+            }
 
-        flippedCard = true;
+         flippedCard = true;
 
 
-      }
+       }
 
-    }
+     }
 
-  }
+   }
+
+
+ }
+
+startGame();
