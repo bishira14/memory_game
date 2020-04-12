cf8af4f9ed751a761e77b8450d816390e3ae7e03
Hamza Bishira <bishira12@hotmail.co.uk>
2020-04-12 17:24:38 +0100

    latest update

--- index.js
+++ index.js
@@ -5,14 +5,22 @@
   let lockedBoard = true;
   let score = 0;
   let scoreElement = document.querySelector('.info h2 span');
+  let win = [];
 
   let timerSeconds = 59;
   let timerMinutes = 1;
   let secondsElement = document.querySelector('.timer .seconds');
   let minutesElement = document.querySelector('.timer .minutes');
+  let showFinish = document.querySelector('.finish');
+  let finishScore = document.querySelector('.finishScore');
+  let finishTime = document.querySelector('.finishTime');
 
   setInterval(function(){
-
+      if(win.length == 6) {
+        finishScore.innerHTML = score;
+        showFinish.style.display = 'flex';
+        return;
+      }
       timerSeconds--;
       if(timerSeconds < 10){
        secondsElement.innerHTML = '0' + timerSeconds;
@@ -21,14 +29,22 @@
      }
       if(timerSeconds == 0){
         timerMinutes--;
-        minutesElement.innerHTML = timerMinutes;
-        timerSeconds = 59; }
+        setTimeout(function(){
+          return minutesElement.innerHTML = timerMinutes;
 
+        },1000)
+        timerSeconds = 59;
+      }
     if(timerMinutes < 0){
       minutesElement.innerHTML = '0';
-      return secondsElement.innerHTML = '00';
-    }
+      secondsElement.innerHTML = '00';
+      setTimeout(function(){
+        finishScore.innerHTML = score;
+        showFinish.style.display = 'flex';
+        return;
+      },1000);
 
+    }
 
   },1000);
 
@@ -60,7 +76,8 @@
               secondCard.removeEventListener('click', flipCard);
               score++;
               scoreElement.innerHTML = score;
-              colorChange()
+              colorChange(scoreElement);
+              win.push('win');
               setTimeout(function(){lockedBoard = true},200);
             }else{
               setTimeout(function(){
@@ -68,7 +85,7 @@
                 secondCard.classList.remove('flip');
                 score--;
                 scoreElement.innerHTML = score;
-                colorChange()
+                colorChange(scoreElement);
                 lockedBoard = true;
               }, 1000);
 
@@ -78,17 +95,18 @@
 
 
        }
-      function colorChange(){
+      function colorChange(element){
         if(score > 0){
-          scoreElement.style.color = 'rgb(60, 119, 60)';
+          element.style.color = 'rgb(60, 119, 60)';
         }else if(score == 0){
-          scoreElement.style.color = 'white';
+          element.style.color = 'white';
         }else{
-          scoreElement.style.color = 'rgb(255, 0, 0)';
+          element.style.color = 'rgb(255, 0, 0)';
         }
 
       }
-
+      //colorchange for finish
+      colorChange(finishScore);
      }
 
    }
